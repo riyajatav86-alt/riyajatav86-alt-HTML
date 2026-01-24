4cb48fd110bdc2bb2f695a2e3210846fb3dd25ec
Riya jatav <233461719+riyajatav86-alt@users.noreply.github.com>
2026-01-24 21:58:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ console.log("Hello, Riya.!");
 
 let btn = document.querySelector("button");
 let body = document.querySelector("body");
-
+isdark = true;
 btn.addEventListener("click", function() {
     //console.log("Button clicked");//
-    body.style.backgroundColor = "red";
+   // body.style.backgroundColor = "red";//
+   if (isdark) {
+    body.style.backgroundColor = "blue";
+   } else {
+    body.style.backgroundColor = "black";
+    
+   }
+   isdark = !isdark;
 })
